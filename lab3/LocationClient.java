96c3e23363bbc12bc5d75f8dc64e0736c85e759f
Saii <60312153+Saii88@users.noreply.github.com>
2025-04-07 12:21:28 +0300

    Create LocationClient.java

---
+++ lab3/LocationClient.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class LocationClient {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Введите количество строчек и столбцов: ");
+
+        int x = in.nextInt();
+        int y = in.nextInt();
+        Location location = new Location(x, y);
+        double[][] array = new double[x][y];
+
+        System.out.println("Введите массив: ");
+
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                array[i][j] = in.nextInt();
+
+            }
+        }
+        location.locatedLargest(array);
+    }
+}
