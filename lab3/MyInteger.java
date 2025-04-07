5b3bc291f46c1b44b232071e3063fb679c61a976
Saii <60312153+Saii88@users.noreply.github.com>
2025-04-07 12:23:07 +0300

    Create MyInteger.java

---
+++ lab3/MyInteger.java
@@ -0,0 +1,59 @@
+public class MyInteger {
+    private int value;
+
+    public MyInteger(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public boolean isEven() {
+        return isEven(value);
+    }
+
+    public boolean isOdd() {
+        return isOdd(value);
+    }
+
+    public boolean isPrime() {
+        return isPrime(value);
+    }
+
+    public static boolean isEven(int value) {
+        return value % 2 == 0;
+    }
+
+    public static boolean isOdd(int value) {
+        return value % 2 == 1;
+    }
+
+    public static boolean isPrime(int value) {
+        if (value < 2) {
+            return false;
+        }
+        for (int i = 2; i * i <= value; i++) {
+            if (value % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean equals(int value) {
+        return this.value == value;
+    }
+
+    public boolean equals(MyInteger myInteger) {
+        return this.value == myInteger.getValue();
+    }
+
+    public static int parseInt(char[] chars) {
+        return Integer.parseInt(new String(chars));
+    }
+
+    public static int parseInt(String str) {
+        return Integer.parseInt(str);
+    }
+}
