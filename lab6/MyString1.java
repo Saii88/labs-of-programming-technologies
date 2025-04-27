6c5a2c37790131cadd4b03f7e6c5bd6a93a463f9
Saii <60312153+Saii88@users.noreply.github.com>
2025-04-27 12:27:51 +0300

    Update MyString1.java

--- lab6/MyString1.java
+++ lab6/MyString1.java
@@ -1,4 +1,29 @@
 public class MyString1 {
+    public static void main(String[] args) {
+        char[] test = {'Т', 'е', 'С', 'т'};
+        char[] test2 = {'T', 'e', 'S', 't', '2'};
+        MyString1 myString1 = new MyString1(test);
+        MyString1 myString2 = new MyString1(test2);
+        System.out.println("myString1.charAt(): " + myString1.charAt(1));
+        System.out.println("myString1.length(): " + myString1.length());
+        System.out.println("myString1.substring(): ");
+        MyString1 substr = myString1.substring(2,4);
+        for (int i = 0; i < substr.length(); i++) {
+            System.out.print(substr.charAt(i));
+        }
+        System.out.println("myString1.lowercase():");
+        MyString1 lower = myString1.toLowerCase();
+        for (int i = 0; i < lower.length(); i++) {
+            System.out.print(lower.charAt(i));
+        }
+        System.out.println("myString1.equals(myString2): " + myString1.equals(myString2));
+        System.out.print("myString1.valueof(): ");
+        MyString1 value = myString1.valueOf(145);
+        for (int i = 0; i < value.length(); i++) {
+            System.out.print(value.charAt(i));
+        }
+    }
+    
     private char[] chars;
 
     public MyString1(char[] chars) {
